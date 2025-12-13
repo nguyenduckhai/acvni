a54e91dec3f395b2422ec349086e0a255e9109d6
khai <khai.nguyen@bubbleran.com>
2025-12-13 04:15:07 +0100

    (feat): new logo and theme

--- script.js
+++ script.js
@@ -451,11 +451,11 @@ document.addEventListener('DOMContentLoaded', () => {
     window.addEventListener('scroll', () => {
         if (window.scrollY > 50) {
             header.style.padding = '10px 0';
-            header.style.backgroundColor = 'rgba(10, 10, 10, 0.98)';
+            header.style.backgroundColor = 'rgba(1, 69, 44, 0.98)';
             header.style.boxShadow = '0 2px 10px rgba(0,0,0,0.3)';
         } else {
             header.style.padding = '15px 0';
-            header.style.backgroundColor = 'rgba(10, 10, 10, 0.95)';
+            header.style.backgroundColor = 'rgba(1, 69, 44, 0.95)';
             header.style.boxShadow = 'none';
         }
     });
